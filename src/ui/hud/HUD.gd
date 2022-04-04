26803d554ba6679d18762a6b67214fb1b4e8e62a
dysphoricUnicorn <git@dysphoric.dev>
2022-04-04 18:36:49 +0200

    Add a better game over screen
    Closes #20

--- src/ui/hud/HUD.gd
+++ src/ui/hud/HUD.gd
@@ -6,6 +6,9 @@ onready var game_over_screen := $GameOverScreen as Panel
 onready var player1_score_label := $Player1ScoreLabel as Label
 onready var player2_score_label := $Player2ScoreLabel as Label
 
+var player1_score:int = 0
+var player2_score:int = 0
+
 # Game state
 var total_player_count := 0
 
@@ -13,6 +16,7 @@ var total_player_count := 0
 func _ready() -> void:
     # Hide game over screen
     game_over_screen.visible = false
+    game_over_screen.set_process_input(false)
 
 
 func init_hud(_total_player_count: int) -> void:
@@ -40,9 +44,35 @@ func set_player_score(score: int, player_number: int) -> void:
 
     if total_player_count > 1:
         score_label.text = "Player %d - Score: %d" % [player_number, score]
+        if player_number == 1:
+            player1_score = score
+        else:
+            player2_score = score
     else:
         score_label.text = "Score: %d" % [score]
 
-
-func show_game_over() -> void:
+func show_game_over(singleplayer: bool, player_number: int) -> void:
+    $GameOverScreen/PlayAgainButton.grab_focus()
+    game_over_screen.set_process_input(true)
+    if singleplayer:
+        $GameOverScreen/TextContainer/WhoWonDisp.visible = false
+        $GameOverScreen/TextContainer/Spacer2.visible = false
+        $GameOverScreen/TextContainer/ScoreDisp.text = get_player_score_label(1).text
+    else:
+        var who_won: int
+        var score: int
+        if player_number == 1:
+            who_won = 2
+            score = player2_score
+        else:
+            who_won = 1
+            score = player1_score
+        $GameOverScreen/TextContainer/WhoWonDisp.text = "Player %d won" % who_won
+        $GameOverScreen/TextContainer/ScoreDisp.text = "Score: %s" % score
     game_over_screen.visible = true
+
+func _on_PlayAgainButton_pressed() -> void:
+    get_tree().reload_current_scene()
+
+func _on_BackToMenuButton_pressed() -> void:
+    get_tree().change_scene_to(load("res://screens/title_screen/TitleScreen.tscn"))
